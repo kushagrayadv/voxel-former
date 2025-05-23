849ddb3458dcab14ae14603a5e8c60b22bf26456
Kushagra Yadav <ky2684@nyu.edu>
2025-05-23 12:21:19 -0400

    removed scripts

--- Downstream/inference.sh
+++
@@ -1,8 +0,0 @@
-instance_dir=$1
-shift 1
-# echo python inference.py --config-path $config_path hydra.job_logging.handlers.file.filename=infernece.log $@
-python inference.py --config-dir ${instance_dir}/.hydra \
-    'hydra.job_logging.handlers.file.filename=infernece.log' \
-    "instance_dir=${instance_dir}" \
-    'hydra.run.dir=${instance_dir}' \
-    $@
